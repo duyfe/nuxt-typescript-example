91c29bd97908287f53590fe58ebccaa475a90f13
duy-gnt <duy.pn@gnt.com.vn>
2021-04-08 11:39:34 +0700

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'semi': ['error', 'never'],
+    'space-before-function-paren': ['error', 'always'],
+    'max-len': 'off',
+    'linebreak-style': 'off',
+    'require-jsdoc': 'off',
+    'no-irregular-whitespace': 'off',
+    'no-invalid-this': 'off',
+    'object-curly-spacing': ['error', 'always']
+  }
+}
